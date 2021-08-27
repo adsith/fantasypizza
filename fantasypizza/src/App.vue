792eb71cfcb8b1bbbf4611eef74534b916b1701b
Carlos Eduardo Álvarez Araguacaima <carlosalvarez1512@gmail.com>
2021-08-27 14:08:32 +0200

    feat: Add base project

---
+++ fantasypizza/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <HelloWorld msg="Comming Soon..."/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return{
+      bodyStyle:{
+        backgroundColor: "#5ABCB9"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
